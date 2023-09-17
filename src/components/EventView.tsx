9c00e726299f489b0caee8bad92d46c29e964bab
KomisarzRyba <antek.olesik@gmail.com>
2023-09-16 22:54:35 -0400

    event page

--- src/components/EventView.tsx
+++ src/components/EventView.tsx
@@ -1,38 +1,51 @@
+"use client";
+
+import { Driver, Event, Rider } from "@prisma/client";
 import axios from "axios";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
+import { useQuery } from "react-query";
 import { EventRegister } from "./EventRegister";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 type EventViewProps = {
   eventName: string;
 };
 
+type EventFatData = Event & { drivers: Driver[] } & { riders: Rider[] };
+
+let role: "driver" | "rider" | null = null;
 export const EventView: FC<EventViewProps> = ({ eventName }) => {
-  const [role, setRole] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get(`/api/role?event=${eventName}`);
-
-        if (data.riders && data.riders[0]) {
-          setRole("rider");
-        } else if (data.drivers && data.drivers[0]) {
-          setRole("driver");
-        } else setRole(null);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        // Handle error appropriately
-      }
-    };
-
-    fetchData();
-  }, [eventName]);
+  const getEventData = async () => {
+    const { data } = await axios.get(`/api/role?event=${eventName}`);
+    return data as EventFatData;
+  };
+  const { data } = useQuery({
+    queryFn: getEventData,
+    onSuccess: (data) => {
+      if (data.drivers[0]) role = "driver";
+      else if (data.riders[0]) role = "rider";
+      else role = null;
+    },
+  });
 
   if (!role) {
+    return <EventRegister eventName={eventName} />;
+  } else
     return (
-      <main>
-        <EventRegister eventName={eventName} />
-      </main>
-    ); // or any loading state
-  } else return <main>{role}</main>;
+      <section className="w-full h-full p-12">
+        <Card className="w-full h-full">
+          <CardHeader>
+            <CardTitle>{data?.name}</CardTitle>
+            <CardDescription>You are a {role} for this event.</CardDescription>
+          </CardHeader>
+          <CardContent>map</CardContent>
+        </Card>
+      </section>
+    );
 };
